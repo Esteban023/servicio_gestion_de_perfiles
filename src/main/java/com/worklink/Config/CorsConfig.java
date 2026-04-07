02ebb32af5a23854017b32a2ceee9fdbf6d7bdae
Esteban Betancourt Lozano <esteban.betancourt.lozano@gmail.com>
2026-04-07 10:50:59 -0500

    Accept incoming

--- src/main/java/com/worklink/Config/CorsConfig.java
+++ src/main/java/com/worklink/Config/CorsConfig.java
@@ -1,4 +1,8 @@
+<<<<<<<< HEAD:src/main/java/com/worklink/Config/CorsConfig.java
 package com.worklink.Config;
+========
+package com.worklink.profile_service.config;
+>>>>>>>> 36c4bb0433cc5d187071dba3b6fda2170885531d:src/main/java/com/worklink/profile_service/config/CorsConfig.java
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +17,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                    .allowedOrigins("http://localhost:5173")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
-    
 }
